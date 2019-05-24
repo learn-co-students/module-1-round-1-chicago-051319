7f4645abdb7081ed2522b0db8873b157812fcbb7
MS-Izoku <spicuzza157@gmail.com>
2019-05-24 08:46:44 -0700

    code challenge done

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,27 @@
 require 'bundler/setup'
 Bundler.require
 require_rel '../app'
+
+a = Review.new("al's beef" , "kewl" , 6 , "zippy")
+b = Review.new("al's beef" , "kewl" , 6 , "zippy")
+c = Review.new("al's beef" , "kewl" , 6 , "zippy")
+
+
+bill = Customer.new("bill" , "barter")
+bal = Customer.new("bill" , "ball")
+chris = Customer.new("Chris" , "jerhericho-ko")
+#p bill
+bill.add_review("kats" , "message!" , 9)
+#p Review.all
+a = Customer.find_all_by_first_name("bill")
+p a
+#p bal
+puts "-------------------------------"
+
+als = Restaurant.new("al's beef")
+bdubs = Restaurant.new("Bdubs")
+
+#a = Restaurant.find_by_name("als")
+p als.reviews
+p als.customers
+p als.average_customer_review
